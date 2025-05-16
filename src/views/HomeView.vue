3fe31c3be51cb64b13167ace2c19da211c9e389a
Brayan <brayanmwangi732@gmail..com>
2025-05-16 16:02:12 +0300

    landing page1

---
+++ src/views/HomeView.vue
@@ -0,0 +1,53 @@
+<script setup>
+
+</script>
+
+<template>
+ <v-container fluid class="ma-0 pa-0">
+  
+  <v-row no-gutters class="ma-0 pa-0 ">
+     
+     
+  <v-col cols="6" class="bg-brown-lighten-4 ma-0 pa-0" >
+      <div class="d-flex flex-column align-center justify-center mr-11">
+  
+    <v-img src="./public/lee_and_associates_logo.svg" width="230px" class="mt-8"></v-img>
+   <div class="mt-5 ml-15">
+         <v-img src="./public/Glenda-Koda-Web-Background-3-1024x533.jpg" class="rounded-t-xl" width="420px" heigth="590px"></v-img>
+       </div>
+</div>
+  </v-col>
+
+  
+  <v-col cols="6" class="bg-blue-grey-darken-4 ma-0 pa-0" >
+    <div class="pa-16">
+    <div class="d-flex flex-column align-center justify-center">
+ <p class="text-subtitle-2">Company profile</p>
+ </div>
+ 
+ <v-divider :thickness="2" class="mt-3"></v-divider>
+ <div class="d-flex flex-column align-center justify-center ">
+ <p class="text-subtitle-2 mt-2">Services</p>
+ </div>
+ <v-divider :thickness="2" class="mt-3"></v-divider>
+  <div class="d-flex flex-column align-center justify-center">
+ <p class="text-subtitle-2 mt-2">The Team</p>
+ </div>
+ <v-divider :thickness="2" class="mt-3"></v-divider>
+ <div class="d-flex flex-column align-center justify-center">
+ <p class="text-subtitle-2 mt-2">Listings</p>
+ </div>
+ <v-divider :thickness="2" class="mt-3"></v-divider>
+  <div class="d-flex flex-column align-center justify-center">
+<p class="text-subtitle-2 mt-2">The blogs</p>
+</div>
+<v-divider :thickness="2" class="mt-3"></v-divider>
+<div class="d-flex flex-column align-center justify-center">
+<p class="text-subtitle-2 mt-4">Contact Us</p>  
+</div>
+</div>
+  </v-col>
+
+  </v-row>
+ </v-container>
+</template>
