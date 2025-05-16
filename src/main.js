5aff7a7c8bf96a3b39f9ca7933c27daa840e07f7
Brayan <brayanmwangi732@gmail..com>
2025-05-16 12:09:07 +0300

    second vuetify project

---
+++ src/main.js
@@ -0,0 +1,28 @@
+import { createApp } from 'vue'
+
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import { createPinia } from 'pinia'
+import router from './router'
+import  '@mdi/font/css/materialdesignicons.css'
+// ...existing code...
+// Components
+import App from './App.vue'
+
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.use(vuetify)
+
+app.mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,42 @@
+<script setup>
+
+</script>
+
+<template>
+ <v-app>
+  
+ <v-container class="text-center">
+  <v-row class="ma-0 pa-0">
+    <v-col cols="6" class="bg-blue d-flex flex-column justify-center align-center"> 
+      
+      <v-img src="./v-logo-circle.png"
+      width="300px"
+      cover></v-img>
+      <h1 class="text-center">Vuetify Company</h1>
+    </v-col>
+    <v-col cols="6" class="d-flex flex-column justify-center align-center">
+      <div>
+      <h2>Brayan mwangi</h2>
+      <p>CEO, Vuetify Company</p>
+      </div>
+      <div class="d-flex pt-4" >
+      <v-icon icon="mdi-phone" class="mr-2"></v-icon>
+     <p>0707943857</p>
+     </div>
+     <div class="d-flex pt-4">
+     <v-icon icon="mdi-map-marker" class="mr-2"></v-icon>
+     <p>Nairobi Kenya</p>
+     </div>
+     <div class="d-flex pt-4">
+     <v-icon icon="mdi-email" class="mr-2"></v-icon>
+     <p>brayanmwangi732@gmail.com</p>
+
+     </div>
+      
+      
+
+    </v-col>
+  </v-row>
+ </v-container>
+ </v-app>
+</template>
